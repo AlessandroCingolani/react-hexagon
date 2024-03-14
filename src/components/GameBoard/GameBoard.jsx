5e86d07b86f6c79646bd52d54d8b314833c390c2
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 21:54:04 +0100

    [add]:test timeout for memorize numbers and isStart for display game table

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,12 +1,16 @@
 import { increment } from "../../redux/counterSlice";
 import { decrement } from "../../redux/counterSlice";
 import { generateRandom } from "../../redux/boardSlice";
+import { generateTotal } from "../../redux/boardSlice";
+import { useEffect } from "react";
 import "./GameBoard.scss";
 import { useSelector, useDispatch } from "react-redux";
 
 function GameBoard() {
   const points = useSelector((state) => state.counter.value);
   const board = useSelector((state) => state.board.value);
+  const isStart = useSelector((state) => state.board.startGame);
+  const sumNumber = useSelector((state) => state.board.sumNumber);
   const dispatch = useDispatch();
 
   function takeValue(cell, rowIndex, cellIndex) {
@@ -18,25 +22,36 @@ function GameBoard() {
     }
   }
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(generateTotal());
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main>
+      <h1>{sumNumber}</h1>
       <button onClick={() => dispatch(generateRandom())}> Genera random</button>
-      <h2 className="text-center">punteggio:{points}</h2>
-      <div id="game-board" className="hex-container">
-        {board.map((row, rowIndex) => (
-          <div key={rowIndex} className="row">
-            {row.map((cell, cellIndex) => (
-              <div
-                key={cellIndex}
-                onClick={() => takeValue(cell, rowIndex, cellIndex)}
-                className="hexagon d-flex justify-content-center align-items-center"
-              >
-                <span>{cell}</span>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+      {isStart ? (
+        <div id="game-board" className="hex-container">
+          <h2>Punteggio: {points}</h2>
+          {board.map((row, rowIndex) => (
+            <div key={rowIndex} className="row">
+              {row.map((cell, cellIndex) => (
+                <div
+                  key={cellIndex}
+                  onClick={() => takeValue(cell, rowIndex, cellIndex)}
+                  className="hexagon d-flex justify-content-center align-items-center"
+                >
+                  <span>{cell}</span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : null}
     </main>
   );
 }
