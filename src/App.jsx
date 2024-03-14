c65da22ba80c0fc53b1ff99ccbe939b1dc6902fa
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 21:53:07 +0100

    [del]:start game

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import "./app.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Title from "./components/Title/Title";
 import GameBoard from "./components/GameBoard/GameBoard";
 
 function App() {
-  const [startGame, setGame] = useState(false);
-
   return (
     <div className="main-container">
       <div className="container">
-        <Title startGame={setGame}></Title>
+        <Title></Title>
         <GameBoard></GameBoard>
-        <button
-          onClick={() => setGame((startGame) => (startGame = !startGame))}
-        >
-          cliccaaa
-        </button>
       </div>
     </div>
   );
