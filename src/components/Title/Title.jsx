8df12ae08d775fc6e823f4e4d4195c0f983a15b1
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 21:53:25 +0100

    [add]:dispatch start game

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,18 +1,30 @@
-import './Title.scss'
+import "./Title.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { startGame } from "../../redux/boardSlice";
 
 function Title() {
-
+  const isStart = useSelector((state) => state.board.startGame);
+  const dispatch = useDispatch();
 
   return (
-   
     <header>
       <h1>Hexagon</h1>
-      <p className='spiegazione'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi labore commodi placeat quas. Nobis suscipit soluta est tenetur obcaecati, optio vel sed, nostrum consectetur aspernatur architecto, totam ipsum consequuntur. Porro.</p>
-      <button className="btn btn-success">Inizia partita</button>
+      <p className="spiegazione">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi labore
+        commodi placeat quas. Nobis suscipit soluta est tenetur obcaecati, optio
+        vel sed, nostrum consectetur aspernatur architecto, totam ipsum
+        consequuntur. Porro.
+      </p>
+      {!isStart ? (
+        <button
+          onClick={() => dispatch(startGame())}
+          className="btn btn-success"
+        >
+          Inizia partita
+        </button>
+      ) : null}
     </header>
-  
-    
-  )
+  );
 }
 
-export default Title
+export default Title;
