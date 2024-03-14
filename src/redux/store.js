559f031f36dccb8eb33b1569a35d9e19b088c864
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 15:48:09 +0100

    [add]:board reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./counterSlice";
+import { boardReducer } from "./boardSlice";
 
 export default configureStore({
   reducer:{
-    counter:counterReducer
+    counter:counterReducer,
+    board:boardReducer
   }
 })
