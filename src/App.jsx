fd662e7191c8014e4d6e3fd4836b8043d8a1d347
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 14:27:21 +0100

    [add]:state true or false test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import './app.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Title from './components/Title/Title';
-import GameBoard from './components/GameBoard/GameBoard';
-
+import { useState } from "react";
+import "./app.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Title from "./components/Title/Title";
+import GameBoard from "./components/GameBoard/GameBoard";
 
 function App() {
-
+  const [startGame, setGame] = useState(false);
 
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <div className="container">
-       <Title></Title>
-       <GameBoard></GameBoard>
+        <Title startGame={setGame}></Title>
+        <GameBoard></GameBoard>
+        <button
+          onClick={() => setGame((startGame) => (startGame = !startGame))}
+        >
+          cliccaaa
+        </button>
       </div>
-    </div> 
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
