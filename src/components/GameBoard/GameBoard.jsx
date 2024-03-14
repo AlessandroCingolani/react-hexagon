5ed89211b5d3c0147fe173d3446d575993b5224a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-14 12:54:56 +0100

    [add]:function generate random number

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,46 +1,84 @@
-import './GameBoard.scss'
+import "./GameBoard.scss";
+
+function generateRandom() {
+  const randomNumber = Math.floor(Math.random() * 12) + 1;
+  return randomNumber;
+}
 
 function GameBoard() {
-
-
   return (
-   
     <main>
-      <div id='game-board' className='hex-container'>
-      <div className="row">
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
+      <div id="game-board" className="hex-container">
+        <div className="row">
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
         </div>
         <div className="row">
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
         </div>
         <div className="row">
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
         </div>
         <div className="row">
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
         </div>
         <div className="row">
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
-            <div className="hexagon"></div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
+          <div className="hexagon d-flex justify-content-center align-items-center">
+            <span>{generateRandom()}</span>
+          </div>
         </div>
       </div>
     </main>
-  
-    
-  )
+  );
 }
 
-export default GameBoard
+export default GameBoard;
