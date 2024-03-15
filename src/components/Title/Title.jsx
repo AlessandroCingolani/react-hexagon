96d9f2539693ac6be61de2859609a4acb705e2c4
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-15 14:26:42 +0100

    [add]:div start game only display when game is no started

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -9,19 +9,21 @@ function Title() {
   return (
     <header>
       <h1>Hexagon</h1>
-      <p className="spiegazione">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi labore
-        commodi placeat quas. Nobis suscipit soluta est tenetur obcaecati, optio
-        vel sed, nostrum consectetur aspernatur architecto, totam ipsum
-        consequuntur. Porro.
-      </p>
       {!isStart ? (
-        <button
-          onClick={() => dispatch(startGame())}
-          className="btn btn-success"
-        >
-          Inizia partita
-        </button>
+        <div className="start-game">
+          <p className="instructions">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+            labore commodi placeat quas. Nobis suscipit soluta est tenetur
+            obcaecati, optio vel sed, nostrum consectetur aspernatur architecto,
+            totam ipsum consequuntur. Porro.
+          </p>
+          <button
+            onClick={() => dispatch(startGame())}
+            className="btn btn-success"
+          >
+            Inizia partita
+          </button>
+        </div>
       ) : null}
     </header>
   );
