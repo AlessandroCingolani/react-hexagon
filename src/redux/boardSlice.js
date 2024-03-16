dffc1c6ea0c70914838109abd90561de39dd3942
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-16 16:02:43 +0100

    [add]:selection phase default null at star game become false

--- src/redux/boardSlice.js
+++ src/redux/boardSlice.js
@@ -14,6 +14,7 @@ export const boardSlice = createSlice({
     sumNumber:0,
     startGame:false,
     userSelections: [],
+    selectionPhase:null
   },
   reducers:{
     generateRandom: (state) => {
@@ -27,18 +28,25 @@ export const boardSlice = createSlice({
       // return value updated
       return {...state, value: updatedValue };
     },
+    // number to reach with 3 selection
     generateTotal:(state)=> {
       const updateNumber = Math.floor(Math.random() * 12) + 10
         return {...state ,sumNumber:updateNumber}  
     },
+    // flag to see started game at click btn
     startGame:(state)=> {
        return {...state ,startGame:true}  
     },
+    // save user selections when are different 
     addUserSelection: (state, action) => {
       state.userSelections = [...state.userSelections, action.payload];
       console.log(state.userSelections);
+    },
+    selectionPhase: (state, newValue) => {
+      console.log(newValue.payload);
+      return {...state, selectionPhase: newValue.payload};
     }
   }
 })
-export const {generateRandom,generateTotal,startGame,addUserSelection } = boardSlice.actions
+export const {generateRandom,generateTotal,startGame,addUserSelection, selectionPhase} = boardSlice.actions
 export const boardReducer = boardSlice.reducer
