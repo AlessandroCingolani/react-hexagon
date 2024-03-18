272426cb3f62b09d66c889b22d7cfffe40774cd3
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-18 16:56:13 +0100

    [add]:reset counter and increment stage

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -4,7 +4,8 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name:'Points',
   initialState:{
-    value:0
+    value:0,
+    stage:1
   },
   reducers:{
     increment: (state) => {
@@ -13,9 +14,16 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1
     },
+    resetCounter:(state) => {
+      state.value = 0,
+      state.stage = 1
+    },
+    incrementStageLevel:(state) => {
+      state.stage += 1
+    }
   }
 })
 
-export const {increment,decrement} = counterSlice.actions
+export const {increment,decrement,resetCounter,incrementStageLevel} = counterSlice.actions
 
 export const counterReducer = counterSlice.reducer
