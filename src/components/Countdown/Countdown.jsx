b8b82d389c05d8d424e2c6314f27e112b3aa0e1a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-03-20 14:27:01 +0100

    [add]:dispatch end game if reach level (N)

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -4,10 +4,11 @@ import {
   selectionPhase,
   generateTotal,
   gamePhase,
+  endGame,
 } from "../../redux/boardSlice";
 import { useSelector, useDispatch } from "react-redux";
-function Countdown({ isStart, phaseSelect, phase }) {
-  const [seconds, setSeconds] = useState(5);
+function Countdown({ isStart, phaseSelect, phase, stage, time }) {
+  const [seconds, setSeconds] = useState(time);
   // dispatch
   const dispatch = useDispatch();
 
@@ -38,10 +39,15 @@ function Countdown({ isStart, phaseSelect, phase }) {
       const timer = setInterval(() => {
         setSeconds((prevSeconds) => {
           if (prevSeconds === 0) {
-            clearInterval(timer);
-            dispatch(gamePhase("END_LEVEL"));
-            dispatch(selectionPhase(false));
-            return 0;
+            if (stage === 2) {
+              dispatch(endGame());
+              console.log("end gameeee");
+            } else {
+              clearInterval(timer);
+              dispatch(gamePhase("END_LEVEL"));
+              dispatch(selectionPhase(false));
+              return 0;
+            }
           } else {
             return prevSeconds - 1;
           }
